612426dcdf38acfcbf62179cbb64918607cb6f40
QuangAnhTo <tqa9554@gmail.com>
2024-03-02 16:49:14 +0700

    adding firebase into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import FirebaseProvider from './context/Firebaseprovider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    <FirebaseProvider>
     <App />
-  </React.StrictMode>,
+    </FirebaseProvider>
 )

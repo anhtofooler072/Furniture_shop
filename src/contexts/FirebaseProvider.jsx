41ab100ff431d7ef504835baedda9f55c5918fbf
QuangAnhTo <tqa9554@gmail.com>
2024-03-02 18:10:53 +0700

    add function to get data from firebase

--- src/contexts/FirebaseProvider.jsx
+++ src/contexts/FirebaseProvider.jsx
@@ -6,12 +6,12 @@ import { getFirestore, collection } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBJqxxO7zqExpluNTtMiGWuoWoBCy8uC6Y",
-    authDomain: "cljs92-n1.firebaseapp.com",
-    projectId: "cljs92-n1",
-    storageBucket: "cljs92-n1.appspot.com",
-    messagingSenderId: "648432244731",
-    appId: "1:648432244731:web:0ca492ed603d53cbb8dd5e"
+  apiKey: "AIzaSyBJqxxO7zqExpluNTtMiGWuoWoBCy8uC6Y",
+  authDomain: "cljs92-n1.firebaseapp.com",
+  projectId: "cljs92-n1",
+  storageBucket: "cljs92-n1.appspot.com",
+  messagingSenderId: "648432244731",
+  appId: "1:648432244731:web:0ca492ed603d53cbb8dd5e"
   };
  
 
@@ -26,8 +26,9 @@ export const FirebaseContext = createContext();
 const FirebaseProvider = ({ children }) => {
   const db = getFirestore(app);
   const messCollect = collection(db, "product"); // Specify the collection path here
+  const messItem = collection(db, "productItem"); // Specify the collection path here
   return (
-    <FirebaseContext.Provider value={{ app, messCollect }}>
+    <FirebaseContext.Provider value={{ app, messCollect, messItem }}>
       {children}
     </FirebaseContext.Provider>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import FirebaseProvider from './context/Firebaseprovider.jsx'
+import FirebaseProvider from './contexts/FirebaseProvider.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <FirebaseProvider>

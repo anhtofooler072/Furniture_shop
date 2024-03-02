31f92448ee0c648880ebd779e37f6347482b6e8d
QuangAnhTo <tqa9554@gmail.com>
2024-03-02 10:36:36 +0700

    styling the navbar

--- src/static-components/Navbar.jsx
+++ src/static-components/Navbar.jsx
@@ -1,14 +1,18 @@
+import './Navbar.css' 
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>
+    <div className='Navbar'>
+        <NavLink to="/"><img src="https://websitedemos.net/furniture-shop-04/wp-content/uploads/sites/1116/2022/07/logo-regular.png" alt="logo" /></NavLink>
+        <div className="navButtons">
         <NavLink to="/shop_all">Shop All</NavLink>
         <NavLink to="/decor">Decor</NavLink>
         <NavLink to="/bedroom">Bedroom</NavLink>
         <NavLink to="/living_room">Living Room</NavLink>
         <NavLink to="/office">Office</NavLink>
+        </div>
     </div>
   )
 }

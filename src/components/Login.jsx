4e2b8e1e401b5790b6891fb9c2d721816f08e80c
QuangAnhTo <quangato072@gmail.com>
2024-03-17 23:56:06 +0700

    Add Google Sign-In functionality and update styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { useContext } from "react";
+import { FirebaseContext } from "../contexts/FirebaseProvider";
+import { signInWithPopup, getAuth } from "firebase/auth";
 
 export default function Login() {
+  const auth = getAuth();
+  const { provider } = useContext(FirebaseContext);
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <div className="aboutUs_Header">
@@ -9,10 +24,29 @@ export default function Login() {
           <h3>Login</h3>
         </div>
         <div className="login_section">
-          <button className="button_submit">
-            <FcGoogle />
-            <span>Sign in with Google</span>
-          </button>
+          <div className="login_box">
+            <div className="login_box_input">
+              <input
+                type="text"
+                placeholder="Email"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="login_box_button">
+              <button>Login</button>
+            </div>
+            <div className="login_box_google">
+              <button
+                onClick={signInWithGoogle}
+                className="button_submit">
+                <FcGoogle style={{ fontSize: "25px" }} />
+                <p>Login with Google</p>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>

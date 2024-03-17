73fd157c988b0ed042286e84329f27a3ecd2875a
QuangAnhTo <quangato072@gmail.com>
2024-03-17 23:15:42 +0700

    Add login section with Google sign-in button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <div className="aboutUs_Header">
+        <div className="TitleBox">
+          <h3>Login</h3>
+        </div>
+        <div className="login_section">
+          <button className="button_submit">
+            <FcGoogle />
+            <span>Sign in with Google</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }

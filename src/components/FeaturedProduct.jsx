2a544521376a931f2dbeac10542921a4e63141ee
QuangAnhTo <quangato072@gmail.com>
2024-03-22 22:50:00 +0700

    fix add and view feature products

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import { FaEye } from "react-icons/fa";
 import { PiBasket } from "react-icons/pi";
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function FeaturedProd(props) {
     let [img, setImg] = useState(props.it.img[0])
+    let [count,setCount]=useState(1)
+    let navigate=useNavigate()
 
     let findCategories=(props.it.featrured)
 
@@ -12,9 +15,37 @@ export default function FeaturedProd(props) {
         checkStock='Out of stock'
     }
 
-    const changeColor = (color) => {
+    let [colorCode, setColorCode] = useState(props.it.productColor[0].colorCode)
+    const changeColor = (color,colorCode) => {
+        setColorCode(colorCode)
+        console.log(colorCode)
         setImg(color)
     }
+
+    let addtoCart = () => {
+        if (props.it.stock == 'out of stock') {
+          alert('hết hàng')
+        } else {
+          let cartstorage = []
+          if (localStorage.getItem('yourcart') !== null) {
+            cartstorage = JSON.parse(localStorage.getItem('yourcart'))
+          }
+          let yourcart = {
+            id: props.it.id,
+            color:colorCode,
+            amount: count
+          }
+          let findcart = cartstorage.findIndex((it) => {
+            return it.id == yourcart.id
+          })
+          if (findcart > -1) {
+            cartstorage[findcart].amount += yourcart.amount
+          } else {
+            cartstorage.push(yourcart)
+          }
+          localStorage.setItem('yourcart', JSON.stringify(cartstorage))
+        }
+      }
 
     if(findCategories == 1){
         return (
@@ -22,8 +53,8 @@ export default function FeaturedProd(props) {
                 <img src={img} alt="item" />
                 <p className='sale'>{checkStock}</p>
                 <div className="button-section">
-                <i><PiBasket/></i>
-                <i><FaEye /></i>
+                <i onClick={addtoCart}><PiBasket/></i>
+                <i onClick={()=>{navigate('/productdetail/'+props.it.id)}}><FaEye /></i>
                 </div>
                 <p className="item-name">{props.it.productName}</p>
                 <p className="item-price">${props.it.price}</p>
